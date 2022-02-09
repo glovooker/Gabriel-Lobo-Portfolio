ce8408a4a784325ca6a53299c977ec381c126db3
Gabriel Lobo <globo.coding@gmail.com>
2022-02-08 21:50:00 -0600

    Functional Version, Finally

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,8 +5,8 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("D:\\Gabriel\\Gabriel\\OneDrive - CEDES Don Bosco\\Personal\\Portafolio y CV\\Portfolio\\Gabriel Lobos Portfolio\\.cache\\dev-404-page.js"))),
-  "component---src-pages-404-js": hot(preferDefault(require("D:\\Gabriel\\Gabriel\\OneDrive - CEDES Don Bosco\\Personal\\Portafolio y CV\\Portfolio\\Gabriel Lobos Portfolio\\src\\pages\\404.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("D:\\Gabriel\\Gabriel\\OneDrive - CEDES Don Bosco\\Personal\\Portafolio y CV\\Portfolio\\Gabriel Lobos Portfolio\\src\\pages\\index.js")))
+  "component---cache-dev-404-page-js": hot(preferDefault(require("D:\\Gabriel\\Gabriel\\OneDrive - CEDES Don Bosco\\Personal\\Portafolio y CV\\Portfolio\\Gabriel Lobo Portfolio\\.cache\\dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("D:\\Gabriel\\Gabriel\\OneDrive - CEDES Don Bosco\\Personal\\Portafolio y CV\\Portfolio\\Gabriel Lobo Portfolio\\src\\pages\\404.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("D:\\Gabriel\\Gabriel\\OneDrive - CEDES Don Bosco\\Personal\\Portafolio y CV\\Portfolio\\Gabriel Lobo Portfolio\\src\\pages\\index.js")))
 }
 
